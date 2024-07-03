53a410f732f75dc713699c560a1035bb027a0dd1
Abdullah Qaasim <abdullahqaasim14@gmail.com>
2024-07-03 23:17:56 +0100

    chore: made little corrections✅💃

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
-// display current day
-const currentDayElement = document.querySelector('[data-testid="currentDay"]');
-const dayElement = currentDayElement.querySelector('[data-testid="day"]');
-
-const today = new Date();
-
-const day = today.getDay();
-
-const weekdays = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-
-dayElement.innerHTML = weekdays[day];
-
-// display current time
 const currentTimeElement = document.querySelector(
   '[data-testid="currentTimeUTC"]'
 );
+const currentDayElement = document.querySelector('[data-testid="currentDay"]');
+const dayElement = currentDayElement.querySelector('[data-testid="day"]');
 
-const hours = today.getHours().toString().padStart(2, "0");
-const minutes = today.getMinutes().toString().padStart(2, "0");
-const seconds = today.getSeconds().toString().padStart(2, "0");
+// Function to update and display the current date and time
+function updateClock() {
+  const today = new Date();
+  const day = today.getDay();
+  const weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
-const formattedDateTime = `${weekdays[day]} ${hours}:${minutes}:${seconds}`;
+  const hours = today.getHours().toString().padStart(2, "0");
+  const minutes = today.getMinutes().toString().padStart(2, "0");
+  const seconds = today.getSeconds().toString().padStart(2, "0");
 
-currentTimeElement.innerHTML = formattedDateTime;
+  const formattedDateTime = `${weekdays[day]} ${hours}:${minutes}:${seconds}`;
+
+  currentTimeElement.innerHTML = formattedDateTime;
+  dayElement.innerHTML = weekdays[day];
+}
+
+updateClock();
+
+const intervalId = setInterval(updateClock, 1000);
